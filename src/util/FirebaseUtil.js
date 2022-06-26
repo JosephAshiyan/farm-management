a9c71ba976a97470578c7d9c5e393bebca48c338
JosephAshiyan <josephasiyan@gmail.com>
2022-06-26 23:46:57 +0300

    Firestore Util

--- src/util/FirebaseUtil.js
+++ src/util/FirebaseUtil.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { initializeApp } from "firebase/app";
 
-function FirebaseUtil() {
-  return (
-    <div>F</div>
-  )
-}
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_TORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+// const app = initializeApp(config);
 
-export default FirebaseUtil
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+// export const remoteConfig = firebase.remoteConfig();
+
+
+
+// import React from 'react'
+
+// function FirebaseUtil() {
+//   return (
+//     <div>F</div>
+//   )
+// }
+
+// export default FirebaseUtil
