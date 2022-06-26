ec6bbf0cd232791801935b25f1b2144d572f840f
JosephAshiyan <josephasiyan@gmail.com>
2022-06-26 11:51:16 +0300

    basic user with redux

---
+++ src/features/user.js
@@ -0,0 +1,22 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        value: {
+            name: "",
+            age: 0,
+            email: ""
+    }},
+    reducers: {
+        login: (state, action) =>{
+            state.value=action.payload;
+        },
+        logout: (state) => {
+            state.value = initialStateValue;
+          },       
+    }
+})
+export const { login, logout } = userSlice.actions;
+
+export default userSlice.reducer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "./features/user";
 
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
